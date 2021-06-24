81c03f874c14cd217db7da83e3f6c9ca114db9d7
Larissa Diniz <ladiniz1.9@gmail.com>
2021-06-23 22:17:19 -0300

    Quarto commit

--- main.js
+++ main.js
@@ -30,3 +30,32 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+/*testimonials slider swiper*/
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/*scrollreveal: mostrar elementos quando der scroll na página*/
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text
+#about .image, #about .text,
+#services .header, #services .card,
+#testimonials .header, #testimonials .testimonials,
+#contact .text, #contact .links
+`,
+  { interval: 100 }
+)
