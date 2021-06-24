139f212e9b357e7c3ae0aab81c4afb9c7b84d43e
Larissa Diniz <ladiniz1.9@gmail.com>
2021-06-24 13:05:24 -0300

    Commit com "Voltar ao topo"

--- main.js
+++ main.js
@@ -18,10 +18,9 @@ for (const link of links) {
 
 /*mudar o header da página quando der scroll*/
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
   if (window.scrollY >= navHeight) {
     //scroll é maior que a altura do header
     header.classList.add('scroll')
@@ -29,7 +28,7 @@ window.addEventListener('scroll', function () {
     //menor que a altura do header
     header.classList.remove('scroll')
   }
-})
+}
 
 /*testimonials slider swiper*/
 const swiper = new Swiper('.swiper-container', {
@@ -55,7 +54,25 @@ scrollReveal.reveal(
 #about .image, #about .text,
 #services .header, #services .card,
 #testimonials .header, #testimonials .testimonials,
-#contact .text, #contact .links
+#contact .text, #contact .links,
+footer .brand, footer .social
 `,
   { interval: 100 }
 )
+
+/*botão voltar para o topo*/
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/*when scroll*/
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
