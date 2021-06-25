923f753f1cb49fa522b10ec648b969ecdd377a3e
Larissa Diniz <ladiniz1.9@gmail.com>
2021-06-25 12:48:55 -0300

    Commit sem link

--- main.js
+++ main.js
@@ -17,10 +17,10 @@ for (const link of links) {
 }
 
 /*mudar o header da página quando der scroll*/
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 
 function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
   if (window.scrollY >= navHeight) {
     //scroll é maior que a altura do header
     header.classList.add('scroll')
@@ -37,7 +37,13 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 /*scrollreveal: mostrar elementos quando der scroll na página*/
@@ -51,19 +57,16 @@ const scrollReveal = ScrollReveal({
 
 scrollReveal.reveal(
   `#home .image, #home .text
-#about .image, #about .text,
-#services .header, #services .card,
-#testimonials .header, #testimonials .testimonials,
-#contact .text, #contact .links,
-footer .brand, footer .social
-`,
+  #about .image, #about .text,
+  #services .header, #services .card,#testimonials .header, #testimonials .testimonials,
+  #contact .text, #contact .links,
+  footer .brand, footer .social`,
   { interval: 100 }
 )
 
 /*botão voltar para o topo*/
-
+const backToTopButton = document.querySelector('.back-to-top')
 function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
   if (window.scrollY >= 560) {
     backToTopButton.classList.add('show')
   } else {
@@ -71,8 +74,33 @@ function backToTop() {
   }
 }
 
+/*Menu ativo conforme a seção visível na página*/
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
 /*when scroll*/
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTop()
+  activateMenuAtCurrentSection()
 })
